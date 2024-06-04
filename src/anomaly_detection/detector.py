5b42e07229cb87dffbcb02a588118e7ae678ec06
Sage9705 <edem22kumahor@gmail.com>
2024-06-04 17:45:18 +0100

    Implement AnomalyDetectionPipeline for multi-model anomaly detection

---
+++ src/anomaly_detection/detector.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from typing import Dict, List
+from ..models.base import AnomalyDetector
+from ..models.statistical.zscore import ZScoreDetector
+from ..models.statistical.iqr import IQRDetector
+
+class AnomalyDetectionPipeline:
+    def __init__(self, models: List[AnomalyDetector] = None):
+        if models is None:
+            self.models = [ZScoreDetector(), IQRDetector()]
+        else:
+            self.models = models
+
+    def detect_anomalies(self, data: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
+        results = {}
+        for symbol, df in data.items():
+            symbol_results = pd.DataFrame(index=df.index)
+            for model in self.models:
+                model.fit(df)
+                symbol_results[model.__class__.__name__] = model.detect(df)
+            symbol_results['is_anomaly'] = symbol_results.any(axis=1)
+            results[symbol] = symbol_results
+        return results
